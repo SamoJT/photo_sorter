47121bbfeb4e63a588dd5d090dc755f09790177b
SamoJT <samojt@gmail.com>
2021-05-08 20:46:59 +0100

    Began developing GUI layout - using place().

--- photo_sort/gui.py
+++ photo_sort/gui.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter.filedialog import askdirectory
 
 class Gui:
 
@@ -7,12 +7,38 @@ class Gui:
         self.CANVAS_WIDTH = 800
         self.CANVAS_HEIGHT = 400
         self.master = Tk()
-        self._canvas_init()
         
+        self.rename_btn, self.move_btn, self.both_btn = IntVar(), IntVar(), IntVar()
+        
+        self._canvas_init()
 
     def main_loop(self):
         mainloop()
-
+        
+    def isChecked(self):
+        pass
+        
+    def _check_buttons(self):
+        # state=DISABLED to prevent grey-out
+        frame = LabelFrame(self.master, text="Options").place(x=15, y=5, height=85, width=90)
+        Checkbutton(frame, text="Rename", var=self.rename_btn, command=self.isChecked).place(x=20, y=20)
+        Checkbutton(frame, text="Move", var=self.move_btn, command=self.isChecked).place(x=20, y=40)
+        
+        frame2 = LabelFrame(self.master).place(x=15, y=65, height=35, width=90)
+        Checkbutton(frame2, text="Both", var=self.both_btn, command=self.isChecked).place(x=20, y=70)
+        
+    def _push_buttons(self):
+        Button(self.master, text="Settings", width=11).place(x=15, y=105)
+        Button(self.master, text="Go!", width=11).place(x=15, y=135)
+    
+    def _TODO(self):
+        # Sort button to allow dir selection and display selected
+        # dir to user.
+        d = askdirectory()
+        
     def _canvas_init(self):
-        w = Canvas(self.master, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT)
-        w.pack()
+        self.master.title("Photo Sorter")
+        self.master.geometry(f'{self.CANVAS_WIDTH}x{self.CANVAS_HEIGHT}')
+        self._check_buttons()
+        self._push_buttons()
+        
