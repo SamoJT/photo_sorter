b4c262cd2b0f43ae1de5159a22fa51329bef0179
SamoJT <samojt@gmail.com>
2021-05-11 09:56:44 +0100

    Select directory, display chosen dir, display file count of dir functionality added.

--- photo_sort/gui.py
+++ photo_sort/gui.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import *
 from tkinter.filedialog import askdirectory
 from tkinter.ttk import Progressbar
@@ -9,33 +10,40 @@ class Gui:
         self.CANVAS_HEIGHT = 250
         self.master = Tk()
         
-        self.rename_btn, self.move_btn, self.both_btn = IntVar(), IntVar(), IntVar()
+        self.radio_btn = IntVar()
         self._canvas_init()
 
     def main_loop(self):
         mainloop()
         
-    def isChecked(self):
+    def options(self):
         pass
     
+    def count_files(self, d):
+        pth, ds, fs = next(os.walk(d))
+        return len(fs)
+    
+    def get_dir(self):
+        d = askdirectory()
+        n = self.count_files(d)
+        self._dynamic_labels(False, d, n)
+        
     def _frames(self):
         LabelFrame(self.master, text="Options").place(x=15, y=5, height=85, width=90)
         LabelFrame(self.master).place(x=15, y=65, height=35, width=90)  # Both divider
         LabelFrame(self.master, text="Directory").place(x=150, y=5, height=45, width=300)
         LabelFrame(self.master, text="Output").place(x=300, y=55, height=115, width=150)
         LabelFrame(self.master, text="Enabled Settings").place(x=150, y=55, height=115, width=135)
-        
-
-    def _check_buttons(self):
-        # state=DISABLED to prevent grey-out
-        Checkbutton(self.master, text="Rename", var=self.rename_btn, command=self.isChecked).place(x=20, y=20)
-        Checkbutton(self.master, text="Move", var=self.move_btn, command=self.isChecked).place(x=20, y=40)
-        Checkbutton(self.master, text="Both", var=self.both_btn, command=self.isChecked).place(x=20, y=70)
+    
+    def _radio_buttons(self):
+        Radiobutton(self.master, text="Rename", var=self.radio_btn, value=1, command=self.options).place(x=20, y=20)
+        Radiobutton(self.master, text="Move", var=self.radio_btn, value=2, command=self.options).place(x=20, y=40)
+        Radiobutton(self.master, text="Both", var=self.radio_btn, value=3, command=self.options).place(x=20, y=70)
         
     def _push_buttons(self):
         Button(self.master, text="Settings", width=11).place(x=15, y=105)
         Button(self.master, text="Go!", width=11).place(x=15, y=135)
-        Button(self.master, text="Select dir", width=7).place(x=455, y=17)
+        Button(self.master, text="Select dir", command=self.get_dir, width=7).place(x=455, y=17)
         
     def _static_labels(self):
         Label(self.master, text="Total Files: ").place(x=15, y=190)
@@ -46,10 +54,6 @@ class Gui:
             file_num = 0
         Label(self.master, text=working_dir).place(x=155, y=23)
         Label(self.master, text=file_num).place(x=75, y=190)
-        
-    def _get_dir(self):
-        # d = askdirectory()
-        pass
     
     def _progress_bar(self):
         progress = Progressbar(self.master, orient=HORIZONTAL, length=300, mode="determinate").place(x=150, y=190)
@@ -61,6 +65,6 @@ class Gui:
         self._frames()
         self._static_labels()
         self._dynamic_labels(True, None, None)
-        self._check_buttons()
+        self._radio_buttons()
         self._push_buttons()
         self._progress_bar()
